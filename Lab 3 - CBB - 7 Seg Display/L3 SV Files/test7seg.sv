bd8d31a373c27a44c60117616f35b7f6c2382f0c
jakujobi <akujobi365@gmail.com>
2023-09-20 14:04:06 -0500

    Create test7seg.sv

---
+++ Lab 3 - CBB - 7 Seg Display/L3 SV Files/test7seg.sv
@@ -0,0 +1,15 @@
+/**
+ * This module is used to test the seven-segment display module. It takes in a 4-bit input and a decimal point signal and outputs a 7-bit signal to display the corresponding digit on the seven-segment display.
+ *
+ * @param in 4-bit input signal to display on the seven-segment display.
+ * @param dp Decimal point signal to turn on the decimal point on the seven-segment display.
+ * @param out 7-bit output signal to display the corresponding digit on the seven-segment display.
+ */
+module test7seg(
+    input [3:0] in,
+    output reg [6:0] out
+);
+
+seven_seg seg1(
+    .in(in),
+    .out(out));
