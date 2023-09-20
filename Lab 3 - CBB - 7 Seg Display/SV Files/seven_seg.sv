03679754ee6058220deb06dc7a128cb4b2abe273
jakujobi <akujobi365@gmail.com>
2023-09-20 11:51:14 -0500

    Created seven_seg.sv file

--- Lab 3 - CBB - 7 Seg Display/SV Files/seven_seg.sv
+++ Lab 3 - CBB - 7 Seg Display/SV Files/seven_seg.sv
@@ -3,4 +3,23 @@
 // The description should include at least one “dec416” sub module, and should allow use of the decimal point.
 // The module should have 4 input wires and have outputs so that when it is connected to a seven segment display the hex digits 0-F are displayed.
 
-module seven_seg()
+module seven_seg(
+    input [3:0] in,
+    input dp,
+    output reg [6:0] out
+);
+
+wire [3:0] dec_in;
+assign dec_in = (in > 4'd9) ? 4'd15 : in;
+
+dec416 dec1(.in(dec_in), .out(out));
+
+always @(dp) begin
+    if (dp) begin
+        out[6] = 1'b1;
+    end else begin
+        out[6] = 1'b0;
+    end
+end
+
+endmodule
