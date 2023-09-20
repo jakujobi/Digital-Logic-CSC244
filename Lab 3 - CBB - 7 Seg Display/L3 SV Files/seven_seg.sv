19ddeab2c52737c6178d35c5741861a371d96bac
jakujobi <akujobi365@gmail.com>
2023-09-20 14:07:02 -0500

    Update seven_seg.sv

--- Lab 3 - CBB - 7 Seg Display/L3 SV Files/seven_seg.sv
+++ Lab 3 - CBB - 7 Seg Display/L3 SV Files/seven_seg.sv
@@ -1,4 +1,4 @@
-//Create an SV file named “seven_seg.sv” that contains one SV module name seven_seg.
+// Create an SV file named “seven_seg.sv” that contains one SV module name seven_seg.
 // Write structural or behavioral SV to describe the operation of seven segment display driver.
 // The description should include at least one “dec416” sub module.
 // The module should have 4 input wires and have outputs so that when it is connected to a seven segment display the hex digits 0-F are displayed.
