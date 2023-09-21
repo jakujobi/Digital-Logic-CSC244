216fc5d98a7568015bef6f738201a7d7cb94203f
jakujobi <akujobi365@gmail.com>
2023-09-20 22:17:34 -0500

    Tried using ABCD input

--- Lab 3 - CBB - 7 Seg Display/L3 SV Files/test_decode.sv
+++ Lab 3 - CBB - 7 Seg Display/L3 SV Files/test_decode.sv
@@ -1,7 +1,7 @@
 // This module is used to test the functionality of the dec416 module. It takes in a 4-bit input, an enable signal, and outputs a 16-bit signal.
 module test_decode (
     //input logic [D:C:B:A] in,
-    input logic [3:0] in, // 4-bit input signal
+    input logic A, B, C, D, // 4-bit input signal
     
     input enable, // enable signal
     
@@ -11,9 +11,19 @@ module test_decode (
 
 // Instantiate the dec416 module and connect its input, enable, and output signals to the corresponding signals in this module.
 dec416 dec1 (
-    .in(in), // connect input signal
+    .A(A), .B(B), .C(C), .D(D), // connect input signal
     .enable(enable), // connect enable signal
     .out(out) // connect output signal
 );
 
 endmodule
+
+
+// // Instantiate the dec416 module and connect its input, enable, and output signals to the corresponding signals in this module.
+// dec416 dec1 (
+//     .in(in), // connect input signal
+//     .enable(enable), // connect enable signal
+//     .out(out) // connect output signal
+// );
+
+// endmodule
