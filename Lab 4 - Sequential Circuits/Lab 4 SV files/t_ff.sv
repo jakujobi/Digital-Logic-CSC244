259ad6d06b722c9fd621e1b81dbcef9790d5c0a9
jakujobi <akujobi365@gmail.com>
2023-09-28 09:31:15 -0500

    Recreates in behavioral sv
    Recreates in behavioral sv

--- Lab 4 - Sequential Circuits/Lab 4 SV files/t_ff.sv
+++ Lab 4 - Sequential Circuits/Lab 4 SV files/t_ff.sv
@@ -1,10 +1,38 @@
-module t_ff(input t, input clk, output reg q);
+module t_ff(
+    input logic t,
+    input logic clk,
+    output logic q,
+    output logic q_bar
+);
 
-always @(posedge clk) begin
-    if (t) begin
-        q <= ~q;
-    end
-end
+logic tn, tnandqbar, tnandq, tnandqbarortnandq;
+// tn = not t
+// tnandqbar = tn & q_bar
+// tnandq = tn & q
+// tnandqbarortnandq = tnandqbar | tnandq
+
+assign tn = ~t;
+assign tnandqbar = tn & q_bar;
+assign tnandq = tn & q;
+assign tnandqbarortnandq = tnandqbar | tnandq;
+
+d_latch latch (
+    .d(tn),
+    .clk(clk),
+    .q(q),
+    .q_bar(q_bar)
+);
+
+
+// module t_ff(input t, input clk, output reg q);
+
+// always @(posedge clk) begin
+//     if (t) begin
+//         q <= ~q;
+//     end
+// end
+
+// endmodule
+
 
 endmodule
-
