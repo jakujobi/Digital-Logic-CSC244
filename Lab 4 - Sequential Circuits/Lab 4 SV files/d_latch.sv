7d3084f414d589b217b7dc013b25ea01f02967c0
jakujobi <akujobi365@gmail.com>
2023-09-28 09:05:20 -0500

    Created the quartus projects and implemented the latches

--- Lab 4 - Sequential Circuits/Lab 4 SV files/d_latch.sv
+++ Lab 4 - Sequential Circuits/Lab 4 SV files/d_latch.sv
@@ -1,10 +1,59 @@
-module d_latch(input logic d, enable, output logic q, q_bar);
+module d_latch(
+    input logic d,
+    input logic clk,
+    output logic q,
+    output logic q_bar
+);
 
-    always_comb begin
-        if (enable) begin
-            q = d;
-            q_bar = ~d;
-        end
-    end
+    // assign r = ~d & clk;
+    // assign s = d & clk;
+    // assign  q = ~(r | q_bar);
+    // assign q_bar = ~(s | q);
+
+logic DandCLK, DnandCLK;
+
+assign DandCLK = d & clk;
+assign DnandCLK = ~d & clk;
+
+sr_latch latch (
+    .s (DandCLK), //s: 1-bit input signal
+    .r (DnandCLK), //r: 1-bit input signal
+    .q (q), //q: 1-bit output signal
+    .q_bar (q_bar) //q_bar: 1-bit output signal
+);
+
+// module d_latch(
+//     input logic d,
+//     input logic clk,
+//     output logic q,
+//     output logic q_bar
+// );
+
+//     logic and_out, nand_out;
+
+//     and gate1(d, enable, and_out);
+//     nand gate2(and_out, clk, nand_out);
+//     nand gate3(nand_out, clk, q);
+//     nand gate4(and_out, clk, q_bar);
+
+// endmodule
+
+
+
+// module d_latch(
+//     input logic d,
+//     input logic clk,
+//     input logic enable,
+//     output logic q, q_bar);
+
+//     always_comb begin
+//         if (enable) begin
+//             q = d;
+//             q_bar = ~d;
+//         end
+//     end
+
+// endmodule
+
 
 endmodule
