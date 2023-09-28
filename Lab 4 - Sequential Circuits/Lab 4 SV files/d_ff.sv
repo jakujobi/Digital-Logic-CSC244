b2162639c64c07d928ace301d442fd1e14d635fe
jakujobi <akujobi365@gmail.com>
2023-09-28 08:08:51 -0500

    Added a reset to the D flip flop

--- Lab 4 - Sequential Circuits/Lab 4 SV files/d_ff.sv
+++ Lab 4 - Sequential Circuits/Lab 4 SV files/d_ff.sv
@@ -1,6 +1,17 @@
-module d_ff(input logic D, input logic clk, output logic Q, output logic Q_bar);
-    always_ff @(posedge clk) begin
-        Q <= D;
-        Q_bar <= ~D;
+module d_ff(
+    input logic D,
+    input logic clk,
+    input logic reset,
+    output logic Q,
+    output logic Q_bar);
+
+    always_ff @(posedge clk, posedge reset) begin
+        if (reset) begin
+            Q <= 0;
+            Q_bar <= 1;
+        end else begin
+            Q <= D;
+            Q_bar <= ~D;
+        end
     end
 endmodule
