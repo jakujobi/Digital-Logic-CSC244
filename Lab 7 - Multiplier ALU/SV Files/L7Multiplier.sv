681c08d24745af614c9c730ec6337fb7c392013c
jakujobi <akujobi365@gmail.com>
2023-10-26 09:24:21 -0500

    Update L7Multiplier.sv

--- Lab 7 - Multiplier ALU/SV Files/L7Multiplier.sv
+++ Lab 7 - Multiplier ALU/SV Files/L7Multiplier.sv
@@ -1,12 +1,40 @@
 // Top-level module
 module L7Multiplier(
-    input [7:0] SW, // Switch inputs
-    output [6:0] HEX2, HEX1, HEX0 // 7-segment display outputs
+    input logic [3:0] A, // Switch inputs //
+    input logic [3:0] B, // Switch inputs //
+
+    output logic [6:0] HEXA,
+    output logic [6:0] HEXB,
+    output logic [6:0] HEXP // 7-segment display outputs
 );
-    wire [7:0] product;
-    mult4 my_multiplier(SW[7:4], SW[3:0], product);
-    // Assuming you have a 7-segment decoder module named 'decimal7decoder'
-    decimal7decoder dec2(SW[7:4], HEX2);
-    decimal7decoder dec1(SW[3:0], HEX1);
-    decimal7decoder dec0(product[7:4], HEX0);
+
+wire [7:0] product;
+logic [6:0] asign, bsign, psign;
+
+mult4 my_multiplier(
+    .A(A),
+    .B(B),
+    .P(product) // 8-bit product
+);
+
+// Assuming you have a 7-segment decoder module named 'decimal7decoder'
+decimal7decoder dec2(
+    .SW(A),
+    .numHEX(HEX2),
+    .signHEX(asign)
+);
+
+//Display B in HEX
+decimal7decoder dec1(
+    .SW(B),
+    .numHEX(HEXB),
+    .signHEX(bsign)
+);
+
+decimal7decoder dec0(
+    .SW(product),
+    .numHEX(HEXP),
+    .signHEX(psign)
+);
+
 endmodule
