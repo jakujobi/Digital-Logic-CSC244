1def5d7b3af587dc9f7b0ca6e1626b6260de9520
jakujobi <akujobi365@gmail.com>
2023-10-26 10:27:57 -0500

    Tested the project, ran into issues

---
+++ Lab 7 - Multiplier ALU/L7 Multiplier/output_files/L7Multiplier.cdf
@@ -0,0 +1,13 @@
+/* Quartus Prime Version 22.1std.1 Build 917 02/14/2023 SC Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(10M50DAF484) Path("C:/Users/akujo_2rtofyy/OneDrive - South Dakota State University - SDSU/#SDSU Courses/CSC 244 - Digital Logic/Digital-Logic-CSC244/Lab 7 - Multiplier ALU/L7 Multiplier/output_files/") File("L7Multiplier.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
--- Lab 7 - Multiplier ALU/SV Files/mult4.sv
+++ Lab 7 - Multiplier ALU/SV Files/mult4.sv
@@ -1,17 +1,33 @@
 // Module for 4-bit array multiplier (mult4)
 module mult4(
-    input [3:0] A, // Multiplicands
-    input [3:0] B, // Multipliers
+    input logic [3:0] A, // Multiplicands
+    input logic [3:0] B, // Multipliers
 
     output reg [7:0] P // 8-bit product
 );
     wire [3:0] PP0, PP1, PP2, PP3;
     wire P0, P1, P2, P3;
     
-    PP4 pp0(A, B[0], 4'b0, PP0, P0);
-    PP4 pp1(A, B[1], PP0, PP1, P1);
-    PP4 pp2(A, B[2], PP1, PP2, P2);
+    PP4 pp0(
+        A,
+        B[0],
+        4'b0,
+        PP0,
+        P0
+    );
+
+    PP4 pp1(
+        A,
+        B[1],
+        PP0,
+        PP1,
+        P1
+    );
+
+    PP4 pp2(
+        A,
+        B[2], PP1, PP2, P2);
     PP4 pp3(A, B[3], PP2, PP3, P3);
 
-    assign P = {P3, P2, P1, P0, PP3};
+    assign P = {P3, P2, P1, P0,};
 endmodule
